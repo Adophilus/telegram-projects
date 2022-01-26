07bc13757cf358b371a2b8bb335e26f3563f93b4
Adophilus <uchenna19of@gmail.com>
2022-01-26 05:38:09 +0100

    added support for logging

--- scripts/scrape_members_from_all_groups.py
+++ scripts/scrape_members_from_all_groups.py
@@ -1,21 +1,37 @@
 from TelegramProjects.helpers.functions import loadConfig, loadFile, getChats, getClient, getMembers
+from telethon.errors.rpcerrorlist import ChatAdminRequiredError
 from telethon.tl.types import User
-from os import path
+from datetime import datetime
+from os import mkdir, path
 import asyncio
 import json
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 config = loadConfig()
 
-counter = 0
+entities = []
+res_id = datetime.strftime(datetime.now(), "%Y-%m-%d")
+
+def checkDirs ():
+    folder = path.join("res", "groups", res_id)
+    if (not path.isdirectory(folder)):
+        mkdir(folder)
+
+    folder = path.join("res", "logs", res_id)
+    if (not path.isdirectory(folder)):
+        mkdir(folder)
 
 async def processEntity (client, entity):
-    _members = await getMembers(client, entity)
-    members = []
-    
-    logging.info("Processing members...")
+    try:
+        _members = await getMembers(client, entity)
+        members = []
+    except ChatAdminRequiredError:
+        logging.warning(f"{entity.title} could not be processed. Reason: not allowed to fetch subscribers")
+        return [ False, 0 ]
 
+    checkDirs()
+    
     c = 0
     for member in _members:
         if (not member.is_self):
@@ -40,13 +56,12 @@ async def processEntity (client, entity):
     """
     data = members
 
-    # file_path = path.join("res", "groups", f"group-{str(counter).zfill(4)}.json")
-    file_path = path.join("res", "groups", f"{entity.id}-{entity.title}.json")
-
-    logging.info("Saving file...")
+    file_path = path.join("res", "groups", res_id, f"{entity.id}-{entity.title}.json")
 
     with open(file_path, "w") as fh:
         json.dump(data, fh)
+
+    return [ True, c ]
 
 async def main ():
     client = await getClient(config.accounts[1])
@@ -58,9 +73,24 @@ async def main ():
         if (not isinstance(chat.entity, User)):
             if (chat.entity.title != config.channels[0].name):
                 logging.info(f"Processing: {chat.entity.title}")
-                await processEntity(client, chat.entity)
+
+                (status, processed) = await processEntity(client, chat.entity)
+                entities.append({
+                    "title": chat.entity.title,
+                    "participants": {
+                        "present": chat.entity.participants_present,
+                        "processed": processed
+                    }
+                })
+                
+                logging.info(f"Processed: {entity.title}")
+
+    with open(path.join("res", "logs", res_id, "overview_report.json"), "w") as fh:
+        json.dump(entities, fh)
 
     await client.disconnect()
+
+    logging.info("All done!")
 
 loop = asyncio.new_event_loop()
 loop.run_until_complete(main())
