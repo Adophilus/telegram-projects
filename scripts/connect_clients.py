f76168e65435061f1b19697514e688b0be015d63
Adophilus <uchenna19of@gmail.com>
2022-02-22 00:35:23 +0100

    renamed the connect client script

--- scripts/connect_clients.py
+++ scripts/connect_clients.py
@@ -1,15 +1,17 @@
 from TelegramProjects.helpers.functions import loadConfig, getClient
 import asyncio
 
-async def main ():
+
+async def main():
     config = loadConfig()
-    clients = [ await getClient(account) for account in config.accounts ]
-    
-    if (all(clients)):
+    clients = [await getClient(account) for account in config.accounts]
+
+    if all(clients):
         print("Connected clients!")
 
     for client in clients:
         await client.disconnect()
+
 
 loop = asyncio.new_event_loop()
 loop.run_until_complete(main())
